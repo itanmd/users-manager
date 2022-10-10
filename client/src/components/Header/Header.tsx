83b1f1e944b2a3185302ad7e4ce0945fcd3a3fe1
Itan <itanmoshe@gmail.com>
2022-10-10 18:43:04 +0300

    states added to components

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,18 +5,39 @@ interface HeaderProps {
 }
  
 interface HeaderState {
-    
+    fullName: string;
+    email: string;
 }
  
 class Header extends React.Component<HeaderProps, HeaderState> {
     
+    constructor(props: HeaderProps) {
+        super(props);
+
+    this.state = {
+        fullName: '',
+        email: ''}
+    }
+
+    handleInputChange = (event: React.ChangeEvent<HTMLInputElement> ,fieldName: string) => {
+        this.setState({
+            ...this.state,
+            [fieldName]: event.target.value
+        })
+    }
+
     render() { 
         return ( 
             <div className="d-flex align-items-center p-3 my-4 bg-light">
                 <h5 className="me-auto mb-0">Users</h5>
                 <div className="d-flex">
-                    <input type="text" placeholder="Name" className="form-control" />
-                    <input type="text" placeholder="Email" className="form-control mx-3" />
+                    <input value={this.state.fullName}
+                    onChange={(e) => this.handleInputChange(e, 'fullName')}
+                    type="text" placeholder="Name" className="form-control" />
+                    
+                    <input value={this.state.email}
+                    onChange={(e) => this.handleInputChange(e, 'email')}
+                    type="text" placeholder="Email" className="form-control mx-3" />
                     <button className="btn btn-info text-white">Add</button>
                 </div>
             </div>
--- client/src/components/Users/Users.tsx
+++ client/src/components/Users/Users.tsx
@@ -21,13 +21,25 @@ interface UsersState {
  
 class Users extends React.Component<UsersProps, UsersState> {
     
-    c
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            users: []
+        }
+    }
 
     render() { 
         return (
             <div className="bg-dark bg-opacity-10 border px-2"> 
                 <Header />
-                <Table users={[]} />
+                {
+                    (this.state.users.length === 0) &&
+                    <div className="alert alert-warning" role="alert">
+                        No users to display
+                    </div>
+                }
+                <Table users={this.state.users} />
             </div>
         );
     }
